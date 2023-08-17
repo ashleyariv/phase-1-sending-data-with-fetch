27db2b1f057b8a107aac524a751446513332fb25
Ashley Rivera <ashleyariv@gmail.com>
2023-08-17 16:11:00 -0400

    POST request

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Add your code here
+function submitData(name, email) {
+    fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+            name: 'ashley',
+            email: 'fakemail',
+        }),
+    })
+    .then(response => response.json())
+    .then(object => console.log(object))
+    .catch(function(error) {
+        alert("error message")
+        console.log(error.message)
+    })
+}
